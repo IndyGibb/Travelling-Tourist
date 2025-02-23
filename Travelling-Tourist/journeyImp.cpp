43c30d81ddb93dbdab57b34f163613c3e9c5493d
baileysoard <bailey.soard@asbury.edu>
2025-02-23 13:53:58 -0500

    Update journeyImp.cpp

--- Travelling-Tourist/journeyImp.cpp
+++ Travelling-Tourist/journeyImp.cpp
@@ -8,6 +8,37 @@ void Journey::addDestination(const string& name, double cost, int travelTime) {
     totalTime += travelTime;
 }
 
+bool Journey::isCityInPath(const string& name) const {
+    for (const auto& dest : route) {
+        if (dest.name == name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Journey::replaceCity(const string& oldName, const string& newName, double newCost, int newTravelTime) {
+    for (auto& dest : route) {
+        if (dest.name == oldName) {
+            totalCost = totalCost - dest.cost + newCost;
+            totalTime = totalTime - dest.travelTime + newTravelTime;
+            dest.name = newName;
+            dest.cost = newCost;
+            dest.travelTime = newTravelTime;
+            return true;
+        }
+    }
+    return false;
+}
+
+bool Journey::exceedsBudget(double newCost, double maxBudget) const {
+    return (totalCost + newCost) > maxBudget;
+}
+
+bool Journey::exceedsTime(int newTravelTime, int maxTime) const {
+    return (totalTime + newTravelTime) > maxTime;
+}
+
 void Journey::displayJourney() const {
     cout << "\nJourney Path:" << endl;
     for (const auto& dest : route) {
