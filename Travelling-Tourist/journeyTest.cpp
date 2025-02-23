d2a8ff8473fdd778c9ff07d3ce688df9d9a10e07
IndyGibb <148816223+IndyGibb@users.noreply.github.com>
2025-02-23 15:35:58 -0500

    Created a test program for the journey class.

---
+++ Travelling-Tourist/journeyTest.cpp
@@ -0,0 +1,12 @@
+#include "journey.h"
+#include <iostream>
+
+using namespace std;
+
+main() {
+	Journey journey;
+	journey.addDestination("Paris", 100, 2);
+	journey.addDestination("London", 200, 3);
+	journey.addDestination("New York", 300, 4);
+	journey.displayJourney();
+}
